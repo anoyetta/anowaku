41140cf3140536143ec3703e4369ec8e6d80f13e
anoyetta <anoyetta@gmail.com>
2021-04-13 17:59:09 +0900

    added stdev

--- anowaku.Core/Extensions/EnumerableExtensions.cs
+++ anowaku.Core/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace anowaku
 {
@@ -15,6 +16,29 @@ namespace anowaku
             }
 
             return e;
+        }
+
+        public static double Stdev<T>(this IEnumerable<T> src)
+        {
+            if (!src.Any())
+            {
+                return 0;
+            }
+
+            // Doubleにキャストして処理を進める
+            var doubleList = src.Select(a => Convert.ToDouble(a)).ToArray();
+
+            // 平均値算出
+            double mean = doubleList.Average();
+
+            // 自乗和算出
+            double sum2 = doubleList.Select(a => a * a).Sum();
+
+            // 分散 = 自乗和 / 要素数 - 平均値^2
+            double variance = sum2 / doubleList.Count() - mean * mean;
+
+            // 標準偏差 = 分散の平方根
+            return Math.Sqrt(variance);
         }
     }
 }
